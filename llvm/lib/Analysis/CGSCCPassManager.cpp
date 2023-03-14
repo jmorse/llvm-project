0c74bb02a087b192cccae67fbe7de8c8f1afb9a5
Jeremy Morse <jeremy.morse@sony.com>
2023-03-14 12:42:07 +0000

    Don't count debug-insts as calls in CGSCC

--- llvm/lib/Analysis/CGSCCPassManager.cpp
+++ llvm/lib/Analysis/CGSCCPassManager.cpp
@@ -384,15 +384,18 @@ PreservedAnalyses DevirtSCCRepeatedPass::run(LazyCallGraph::SCC &InitialC,
       CallCount &Count =
           CallCounts.insert(std::make_pair(&N.getFunction(), CountLocal))
               .first->second;
-      for (Instruction &I : instructions(N.getFunction()))
-        if (auto *CB = dyn_cast<CallBase>(&I)) {
-          if (CB->getCalledFunction()) {
-            ++Count.Direct;
-          } else {
-            ++Count.Indirect;
-            CallHandles.insert({CB, WeakTrackingVH(CB)});
+      for (BasicBlock &BB : N.getFunction()) {
+        for (Instruction &I : BB.instructionsWithoutDebug()) {
+          if (auto *CB = dyn_cast<CallBase>(&I)) {
+            if (CB->getCalledFunction()) {
+              ++Count.Direct;
+            } else {
+              ++Count.Indirect;
+              CallHandles.insert({CB, WeakTrackingVH(CB)});
+            }
           }
         }
+      }
     }
 
     return CallCounts;
