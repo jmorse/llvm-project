560ddc147241647f0063c4f1c53e13db747cbcd1
Jeremy Morse <jeremy.morse@sony.com>
2023-11-02 15:06:20 +0000

    Another experimental compile-time-perf fix

--- llvm/lib/IR/Function.cpp
+++ llvm/lib/IR/Function.cpp
@@ -96,10 +96,12 @@ void Function::convertFromNewDbgValues() {
 }
 
 void Function::setIsNewDbgInfoFormat(bool NewFlag) {
+#if 0
   if (NewFlag && !IsNewDbgInfoFormat)
     convertToNewDbgValues();
   else if (!NewFlag && IsNewDbgInfoFormat)
     convertFromNewDbgValues();
+#endif
 }
 
 //===----------------------------------------------------------------------===//
