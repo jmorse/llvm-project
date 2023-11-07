7d34dccec87938dc3f1d111e9d83029b7856c806
Jeremy Morse <jeremy.morse@sony.com>
2023-11-07 13:06:02 +0000

    Revert "iBugsnax"
    This reverts commit fbce46b6180bf1c14f41ab39032567b0e658d289.
    
    Revert "Now try avoiding a massive switch case..."
    
    This reverts commit e34e13e933633c240385b9cf4ffc1700bf387577.

--- llvm/lib/IR/Metadata.cpp
+++ llvm/lib/IR/Metadata.cpp
@@ -448,23 +448,23 @@ void ReplaceableMetadataImpl::resolveAllUses(bool ResolveUsers) {
 }
 
 ReplaceableMetadataImpl *ReplaceableMetadataImpl::getOrCreate(Metadata &MD) {
-  if (auto *Ptr = dyn_cast<ValueAsMetadata>(&MD))
-    return Ptr;
-  else if (auto *Ptr = dyn_cast<DIArgList>(&MD))
-    return Ptr->Context.getOrCreateReplaceableUses();
-  // Otherwise: it's a MDNode.
-  MDNode *N = dyn_cast<MDNode>(&MD);
-  return (!N || N->isResolved()) ? nullptr : N->Context.getOrCreateReplaceableUses();
+  if (auto *N = dyn_cast<MDNode>(&MD)) {
+    bool shouldresolve = isa<DIArgList>(N) || !N->isResolved();
+    if (shouldresolve)
+      return N->Context.getOrCreateReplaceableUses();
+    return nullptr;
+  }
+  return dyn_cast<ValueAsMetadata>(&MD);
 }
 
 ReplaceableMetadataImpl *ReplaceableMetadataImpl::getIfExists(Metadata &MD) {
-  if (auto *Ptr = dyn_cast<ValueAsMetadata>(&MD))
-    return Ptr;
-  else if (auto *Ptr = dyn_cast<DIArgList>(&MD))
-    return Ptr->Context.getReplaceableUses();
-  // Otherwise: it's a MDNode.
-  MDNode *N = dyn_cast<MDNode>(&MD);
-  return (!N || N->isResolved()) ? nullptr : N->Context.getReplaceableUses();
+  if (auto *N = dyn_cast<MDNode>(&MD)) {
+    bool shouldresolve = isa<DIArgList>(N) || !N->isResolved();
+    if (shouldresolve)
+      return N->Context.getReplaceableUses();
+    return nullptr;
+  }
+  return dyn_cast<ValueAsMetadata>(&MD);
 }
 
 bool ReplaceableMetadataImpl::isReplaceable(const Metadata &MD) {
